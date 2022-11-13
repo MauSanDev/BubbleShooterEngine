8804fd5c1fb8a961902da96e69fc21baacc9f5d9
Mauro Sanchez <maurosanchez.work@gmail.com>
2022-11-12 21:04:05 -0300

    Added Line Renderer

--- Assets/Board/BubbleShooterBoard/BubbleShooter.cs
+++ Assets/Board/BubbleShooterBoard/BubbleShooter.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class BubbleShooter : AbstractBoardComponent<BubblePiece, BubbleShooterBoard>
 {
     [SerializeField] private float shotSpeed;
+    [SerializeField] private LineRenderer lineRenderer = null;
     private BubblePiece bubbleToShot = null;
+    private float lineLength = 3f;
 
     public bool CanShot { get; set; } = true;
 
+    private Vector3 GetDirection()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        direction = new Vector3(direction.x, direction.y, 1);
+        direction.Normalize();
+
+        return direction;
+    }
+
     void Update()
     {
+        Vector3 direction = GetDirection();
+        lineRenderer.SetPosition(1, direction * lineLength);
+        
         if (Input.GetMouseButtonDown(0))
         {
             if (!Board.HasRemainingMoves)
@@ -17,14 +32,12 @@ public class BubbleShooter : AbstractBoardComponent<BubblePiece, BubbleShooterBo
                 return;
             }
             
-            ShotBubble();
+            ShotBubble(direction);
         }
     }
 
-    private void ShotBubble()
+    private void ShotBubble(Vector3 direction)
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - transform.position;
         direction.Normalize();
         
         bubbleToShot.ShotBubble(direction * shotSpeed);
